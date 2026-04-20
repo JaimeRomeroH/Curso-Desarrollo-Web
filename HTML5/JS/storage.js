fc3c64f91bccb14024e3c2b1a04d77401462163d
JaimeRomero <jaime_im@outlook.com>
2026-04-20 15:15:29 -0600

    Se agregan los archivos del tema storage

---
+++ HTML5/JS/storage.js
@@ -0,0 +1,17 @@
+//Local storage
+
+//Metodo 1
+//localStorage.nombre = "Jaime Romero";
+
+//Metodo 2
+//localStorage.setItem("nombre", "Jaime Romero");
+
+//Mostrar en pantalla 
+//document.writeln(localStorage.nombre);
+
+//Eliminar elemento
+//localStorage.removeItem("nombre")
+
+//Session storage
+//sessionStorage.nombre = "Jaime Romero";
+document.writeln(sessionStorage.nombre);
